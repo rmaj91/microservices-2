4fd3aa74e58e15ac314fb2b1b0bafedc8994758c
Rafal Maj <rmaj91@gmail.com>
2020-12-12 22:33:47 +0100

    Registering in eureka

--- licensing-service/src/main/java/com/rmaj91/licensingservice/LicensingServiceApplication.java
+++ licensing-service/src/main/java/com/rmaj91/licensingservice/LicensingServiceApplication.java
@@ -2,7 +2,9 @@ package com.rmaj91.licensingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class LicensingServiceApplication {
 
