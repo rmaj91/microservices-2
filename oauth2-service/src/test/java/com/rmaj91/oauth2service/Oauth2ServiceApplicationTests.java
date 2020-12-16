e9f863bda34c1213aa713485fea857c5b9b8d2ed
Rafal Maj <rmaj91@gmail.com>
2020-12-16 21:57:19 +0100

    Add Oauth2 service

---
+++ oauth2-service/src/test/java/com/rmaj91/oauth2service/Oauth2ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmaj91.oauth2service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
